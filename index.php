e679b3cdf0c6b6ef41be12b1cd5f7234fc4f24ad
emonShimoul <emonshimoul@gmail.com>
2023-12-28 18:32:46 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php bloginfo("name"); ?>
+    </title>
+</head>
+<body>
+    <?php
+        while(have_posts()){
+            the_post();
+            
+            echo "<h2>";
+            the_title();
+            echo "</h2>";
+        }
+    ?>
+</body>
+</html>
