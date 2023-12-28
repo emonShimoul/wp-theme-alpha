a8e8c353b465e7f3a692e421a479fb65bef6eafb
emonShimoul <emonshimoul@gmail.com>
2023-12-28 21:10:27 +0600

    display comment form in single post view

--- single.php
+++ single.php
@@ -52,6 +52,13 @@
                         the_content();
                     ?>
                 </div>
+                <?php if(comments_open()): ?>
+                    <div class="col-md-10 offset-md-1">
+                        <?php
+                            comments_template(); 
+                        ?>
+                    </div>
+                <?php endif; ?>
             </div>
 
         </div>
