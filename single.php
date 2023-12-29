7c8305397cc2e666054213c819df9e6dcd600dec
emonShimoul <emonshimoul@gmail.com>
2023-12-29 15:55:06 +0600

    register widget area and displayed on sidebar of single page

--- single.php
+++ single.php
@@ -2,71 +2,87 @@
 
 <body <?php body_class(); ?>>
 <?php get_template_part("hero"); ?>
-<div class="posts">
-<?php
-    while(have_posts()) {
-        the_post();
-?>
-    <div class="post" <?php post_class(); ?> >
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 offset-md-1">
-                    <h2 class="post-title text-center">
-                        <?php the_title(); ?>
-                    </h2>
-                    <p class="text-center">
-                        <strong><?php the_author(); ?></strong><br/>
-                        <?php echo get_the_date(); ?>
-                    </p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-10 offset-md-1">
-                    <p>
-                        <?php
-                            if(has_post_thumbnail()){
-                                the_post_thumbnail("large", array("class='img-fluid'"));
-                            }
-                        ?>
-                    </p>
-                    
-                    <?php
-                        the_content();
 
-                        next_post_link();
-                        echo "<br/>";
-                        previous_post_link();
-                    ?>
-                </div>
-                <?php if(comments_open()): ?>
-                    <div class="col-md-10 offset-md-1">
-                        <?php
-                            comments_template(); 
-                        ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+        <div class="posts">
+            <?php
+                while(have_posts()) {
+                    the_post();
+            ?>
+                <div class="post" <?php post_class(); ?> >
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h2 class="post-title">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <p>
+                                    <strong><?php the_author(); ?></strong><br/>
+                                    <?php echo get_the_date(); ?>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>
+                                    <?php
+                                        if(has_post_thumbnail()){
+                                            the_post_thumbnail("large", array("class='img-fluid'"));
+                                        }
+                                    ?>
+                                </p>
+                                
+                                <?php
+                                    the_content();
+
+                                    next_post_link();
+                                    echo "<br/>";
+                                    previous_post_link();
+                                ?>
+                            </div>
+                            <?php if(comments_open()): ?>
+                                <div class="col-md-10 offset-md-1">
+                                    <?php
+                                        comments_template(); 
+                                    ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+
                     </div>
-                <?php endif; ?>
+                </div>
+            <?php
+                }
+            ?>
+                
+                <div class="container post-pagination">
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-8">
+                            <?php
+                                the_posts_pagination( array(
+                                    "screen_reader_text"=>' ',
+                                    "prev_text"=>"New Posts",
+                                    "next_text"=>"Old Posts"
+                                ) );
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
-    </div>
-<?php
-    }
-?>
-    
-    <div class="container post-pagination">
-        <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-8">
-                <?php
-                    the_posts_pagination( array(
-                        "screen_reader_text"=>' ',
-                        "prev_text"=>"New Posts",
-                        "next_text"=>"Old Posts"
-                    ) );
-                ?>
-            </div>
+        <div class="col-md-4">
+            <?php
+                if(is_active_sidebar("sidebar-1")){
+                    dynamic_sidebar( "sidebar-1" );
+                }
+            ?>
         </div>
     </div>
 </div>
+
+
 
 <?php get_footer(); ?>
