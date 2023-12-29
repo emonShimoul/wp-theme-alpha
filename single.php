cf29bdaf4528d39d89afaf8287d99048872f4aa9
emonShimoul <emonshimoul@gmail.com>
2023-12-29 13:27:00 +0600

    break a theme into small files

--- single.php
+++ single.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<div class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="tagline">
-                    <?php bloginfo( "description" ); ?>
-                </h3>
-                <h1 class="align-self-center display-1 text-center heading">
-                <?php bloginfo( "name" ); ?>
-                </h1>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="posts">
+<?php get_header(); ?>
 
+<body <?php body_class(); ?>>
+<?php get_template_part("hero"); ?>
+<div class="posts">
 <?php
     while(have_posts()) {
         the_post();
@@ -82,16 +64,5 @@
         </div>
     </div>
 </div>
-<div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                &copy; LWHH - All Rights Reserved
-            </div>
-        </div>
-    </div>
-</div>
 
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
