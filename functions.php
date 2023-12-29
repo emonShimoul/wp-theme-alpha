7188a1febd5e5db23088154e21baf681642e8640
emonShimoul <emonshimoul@gmail.com>
2023-12-29 16:49:25 +0600

    manage password protected post

--- functions.php
+++ functions.php
@@ -27,6 +27,44 @@ function alpha_sidebar(){
             'after_title'   => '</h2>',
         )
     );
-}
 
+    register_sidebar(
+        array(
+            'name'      => __('Footer Left', 'alpha'),
+            'id'        => 'footer-left',
+            'description' => __('Widgetized Area On The Left Side', 'alpha'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '',
+            'after_title'   => '',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name'      => __('Footer Right', 'alpha'),
+            'id'        => 'footer-right',
+            'description' => __('Widgetized Area On The Right Side', 'alpha'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '',
+            'after_title'   => '',
+        )
+    );
+}
 add_action( "widgets_init", "alpha_sidebar" );
+
+
+function alpha_the_excerpt($excerpt){
+    if(!post_password_required()){
+        return $excerpt;
+    } else{
+        echo get_the_password_form();
+    }
+}
+add_filter("the_excerpt", "alpha_the_excerpt");
+
+function alpha_protected_title_change(){
+    return "%s";
+}
+add_filter("protected_title_format", "alpha_protected_title_change");
--- index.php
+++ index.php
@@ -31,12 +31,17 @@
                             if(has_post_thumbnail()){
                                 the_post_thumbnail("large", array("class='img-fluid'"));
                             }
+
+                            // if(!post_password_required()){
+                            //     the_excerpt();
+                            // } else{
+                            //     echo get_the_password_form();
+                            // }
+
+                            the_excerpt();
                         ?>
+                        
                     </p>
-                    
-                    <?php
-                        the_excerpt();
-                    ?>
                 </div>
             </div>
 
