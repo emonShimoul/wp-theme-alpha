d61d204bb581836056a5a2da2cfe55fb5acce9f3
emonShimoul <emonshimoul@gmail.com>
2023-12-29 13:38:18 +0600

    discussion about get_template_part

---
+++ pttwo.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<br/>PT Two<br/>";
+get_template_part("templates/pt-three");
+// get_template_part("templates/pt", "three");
---
+++ templates/pt-three.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<br/>PT Three<br/>";
+
+get_template_part("templates/pt-four");
+// get_template_part("templates/pt", "four");
